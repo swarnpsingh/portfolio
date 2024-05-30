2743d4befc1e7f8cb0fb676767080fb1ed5025e4
Swarn Pratap Singh <swarnpratapsingh5@gmail.com>
2024-05-30 16:14:46 +0530

    changes made to profile picutre

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ppf from "./ppf.JPG"
+import ppf from "./logo-new.png"
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
